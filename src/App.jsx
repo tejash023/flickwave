099eec16576fcb628e052728783fcfde578ac093
Tejashwa Rajvardhan <36923392+tejash023@users.noreply.github.com>
2023-10-20 13:46:07 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./index.css";
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold">Hello world!</h1>
+      <h1 className="text-3xl font-bold">FlickWave!</h1>
     </div>
   );
 }
