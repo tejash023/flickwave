b470b362a9973b40f3d2998b7ea30d94a7b66ce6
Tejashwa Rajvardhan <tejashwa.rajvardhan@ibm.com>
2024-02-28 01:05:47 +0530

    added login and browse page components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import "./index.css";
 
+import Body from "./Components/Body";
+
 function App() {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold">Hello world!</h1>
-    </div>
-  );
+  return <Body />;
 }
 
 export default App;
